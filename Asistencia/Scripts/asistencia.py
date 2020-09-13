ed72da7d8bfaf8c9c5bdc5baacb03867cf6c21d6
Giovanni <giovanniloez9808@gmail.com>
2020-09-12 23:54:14 -0500

    [ADD] Asistencia file and script

---
+++ Asistencia/Scripts/asistencia.py
@@ -0,0 +1,28 @@
+import numpy as np
+from os import listdir
+#<-----------------------------Localizacion de archivos------------------>
+dir="../Archivos/"
+#<-----------------------------Archivos---------------------------------->
+files=listdir(dir)
+#<--------------------------Inicializacion de los datos-------------------->
+ids=[];counts=[]
+for file in files:
+    #<---------------------------------------Lectura de asistencias--------------------->
+    data=np.loadtxt(dir+file,skiprows=1,usecols=1,dtype=str,delimiter=",")
+    #<----------------------------Personas nuevas------------------------------>
+    new=list(set(data)-set(ids))
+    #<------------------------Adiccion de las personas nuevas-------------------->
+    counts=np.append(counts,np.zeros(np.size(new)))
+    ids=np.append(ids,new)
+    #<------------------------------numero de personas totales---------------------->
+    n_persons=np.size(ids)
+    for i in range(n_persons):
+        #<----------------------------encontrar el registro----------------------->
+        found=np.size(np.where(ids[i]==data)[0])
+        if found!=0:
+            #<-----------------------------Suma------------------------>
+            counts[i]+=1
+assistance_file=open(dir+"Asistencia.txt","w")
+for id,count in zip(ids,counts):
+    assistance_file.write(id+" "+str(int(count))+"\n")
+assistance_file.close()
