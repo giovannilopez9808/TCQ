005724245bb13580e596f002cd1ba583ed272904
Giovanni <giovannilopez9808@gmail.com>
2020-11-06 00:16:52 -0600

    [ADD] Asistencia 10-31

--- Cuantica/armonicos_esfericos.py
+++ Cuantica/armonicos_esfericos.py
@@ -5,8 +5,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm, colors
-l = 1    #grado del armónico esférico
-m_n = 1    # orden
+l = 4    #grado del armónico esférico
+m_n =2    # orden
 phi, theta = np.mgrid[0:2*np.pi:200j, 0:np.pi:100j]
 #R = np.abs(sp.sph_harm(m, l, phi, theta))
 R = sp.sph_harm(m_n, l, phi, theta).real
--- Cuantica/bloch_graphics.py
+++ Cuantica/bloch_graphics.py
@@ -1,5 +1,10 @@
 from qiskit.visualization import plot_bloch_vector
 import matplotlib.pyplot as plt
-vector=[0,0,1]
-plot_bloch_vector(vector)
+import numpy as np
+up=np.array([0,0,1])
+down=np.array([0,0,-1])
+#Compuerta de Hadamard
+H=1/np.sqrt(0.5)*np.array([[0,0,0],[0,1,1],[0,1,-1]])
+n=np.dot(H,down)/2
+plot_bloch_vector(n)
 plt.show()
